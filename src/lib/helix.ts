045d0a88879b7ee8f9235f35adc12c5222f5f5d7
Jacob Foster <jfost3@gmail.com>
2022-08-31 20:30:16 -0500

    Add badges to Helix

--- src/lib/helix.ts
+++ src/lib/helix.ts
@@ -9,7 +9,15 @@ export async function getUserByLogin(login: string): Promise<{ user: Helix.User;
 	return await makeRequest(`users?login=${login}&user=true`);
 }
 
-namespace Helix {
+export async function getBadgesGlobal(): Promise<Helix.BadgesSimple.BadgesData> {
+	return await makeRequest(`chat/badges/global?simplify=true`);
+}
+
+export async function getBadgesChannel(channelId: string): Promise<Helix.BadgesSimple.BadgesData> {
+	return await makeRequest(`chat/badges?broadcaster_id=${channelId}&simplify=true`);
+}
+
+export namespace Helix {
 	export interface User {
 		id: string;
 		login: string;
@@ -29,5 +37,12 @@ namespace Helix {
 		created_at: string;
 		/** Behind a scope. */
 		email?: string;
+	}
+	export namespace BadgesSimple {
+		export type Badge = Record<string, string>;
+		export type BadgeSet = Record<string, Badge>;
+		export interface BadgesData {
+			badges: BadgeSet;
+		}
 	}
 }
