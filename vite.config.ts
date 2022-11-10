6b551de30a0ca9661cf004237dab87ff1a05d086
Jacob Foster <jfost3@gmail.com>
2022-11-09 22:27:50 -0600

    Update deps and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,27 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://vitejs.dev/config/shared-options.html#base
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
+      '@lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][extname]',
+        entryFileNames: '[name].js',
+      }
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true,
+      useFsEvents: false,
     }
   }
 })
